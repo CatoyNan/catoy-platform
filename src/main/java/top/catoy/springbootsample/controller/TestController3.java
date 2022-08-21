35b9162a40d4c9124c3e454300f8e0eb446f30fd
catoyNan <xjn17858906105@gmail.com>
2022-08-21 22:36:45 +0800

    init

---
+++ src/main/java/top/catoy/springbootsample/controller/TestController3.java
@@ -0,0 +1,40 @@
+package top.catoy.springbootsample.controller;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/")
+public class TestController3 {
+    @GetMapping
+    public String hello1() {
+        return "hello world!1111";
+    }
+
+    public static class Student {
+        int id;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        Date date;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+    }
+}
