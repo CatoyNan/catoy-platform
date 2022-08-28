1e8fd10772bc14b3285284788ecd05c7e56c9ddc
catoyNan <xjn17858906105@gmail.com>
2022-08-28 17:37:29 +0800

    init

--- catoy-platform-music/catoy-platform-music-adapter/src/main/java/top/catoy/web/CustomerController.java
+++ catoy-platform-music/catoy-platform-music-adapter/src/main/java/top/catoy/web/CustomerController.java
@@ -16,12 +16,12 @@ public class CustomerController {
     @Autowired
     private CustomerServiceI customerService;
 
-    @GetMapping("/helloworld")
+    @GetMapping("helloworld")
     public String helloWorld(){
         return "Hello, welcome to COLA world!";
     }
 
-    @GetMapping("/customer")
+    @GetMapping("customer")
     public MultiResponse<CustomerDTO> listCustomerByName(@RequestParam(required = false) String name){
         CustomerListByNameQry customerListByNameQry = new CustomerListByNameQry();
         customerListByNameQry.setName(name);
